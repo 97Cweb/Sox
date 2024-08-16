a93d861b706d87dd6dd6ed78784eb0bbc9430d90
97cweb <97cweb@gmail.com>
2024-08-16 08:52:49 -0400

    stereo audio in, wakeword prediction out

--- Ears/SoxEars2.py
+++ Ears/SoxEars2.py
@@ -2,8 +2,8 @@ import pyaudio
 import numpy as np
 from openwakeword.model import Model as wakeModel
 
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
+FORMAT = pyaudio.paFloat32
+CHANNELS = 2
 RATE = 48000
 CHUNK = 3072  # Increased buffer size, 3*1024 as wee only keep 1/3 for wakeword detection
 audio = pyaudio.PyAudio()
@@ -33,17 +33,57 @@ wakeword_model = wakeModel(
     vad_threshold=0.05
 )
 
+# From https://gist.github.com/HudsonHuang/fbdf8e9af7993fe2a91620d3fb86a182
+def float2pcm(sig, dtype='int16'):
+    """Convert floating point signal with a range from -1 to 1 to PCM.
+    Any signal values outside the interval [-1.0, 1.0) are clipped.
+    No dithering is used.
+    Note that there are different possibilities for scaling floating
+    point numbers to PCM numbers, this function implements just one of
+    them.  For an overview of alternatives see
+    http://blog.bjornroche.com/2009/12/int-float-int-its-jungle-out-there.html
+    Parameters
+    ----------
+    sig : array_like
+        Input array, must have floating point type.
+    dtype : data type, optional
+        Desired (integer) data type.
+    Returns
+    -------
+    numpy.ndarray
+        Integer data, scaled and clipped to the range of the given
+        *dtype*.
+    See Also
+    --------
+    pcm2float, dtype
+    """
+    sig = np.asarray(sig)
+    if sig.dtype.kind != 'f':
+        raise TypeError("'sig' must be a float array")
+    dtype = np.dtype(dtype)
+    if dtype.kind not in 'iu':
+        raise TypeError("'dtype' must be an integer type")
+
+    i = np.iinfo(dtype)
+    abs_max = 2 ** (i.bits - 1)
+    offset = i.min + abs_max
+    return (sig * abs_max + offset).clip(i.min, i.max).astype(dtype)
+
+    
 if __name__ == "__main__":
     didHearName = False
     try:
         while True:
             try:
-                liveAudio = np.frombuffer(mic_stream.read(CHUNK, exception_on_overflow=False), dtype=np.int16)
-                downSampledAudio = liveAudio[0::3]
-                prediction = wakeword_model.predict(downSampledAudio)
-                #print(prediction)
+                liveAudio = np.frombuffer(mic_stream.read(CHUNK, exception_on_overflow=False), dtype=np.float32)
+                stackedAudio = np.stack((liveAudio[::2], liveAudio[1::2]), axis=0)  # channels on separate axes
+                leftTrack = stackedAudio[0]
+                downSampledAudio = leftTrack[0::3] #left track only
+                pcmDownSampledAudio = float2pcm(downSampledAudio)
+                prediction = wakeword_model.predict(pcmDownSampledAudio)
                 
-                if prediction["hey_socks"] >= 0.3:
+                
+                if prediction["hey_socks"] >= 0.1:
                     didHearName = True
                 elif didHearName:
                     didHearName=False
